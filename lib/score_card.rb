dc7a526eba9aa4512ce12419f1fa545962899607
Sourabh Ghorpade <sourabh.ghorpade@runnr.in>
2017-02-16 00:29:44 +0530

    Now we can print over summary as well.

--- lib/score_card.rb
+++ lib/score_card.rb
@@ -1,11 +1,29 @@
 class ScoreCard
-  def initialize(outcomes)
+  BALLS_IN_AN_OVER = 6
+
+  def initialize(number_of_overs, target, outcomes)
+    @total_overs = number_of_overs
+    @overs_left = number_of_overs
+    @target = target
     @outcomes = outcomes
   end
 
   def commentary
-    @outcomes.each_with_index.map do |outcome, ball_number|
-      "0.#{ball_number + 1} #{outcome.to_s}"
+    commentary = (@total_overs * BALLS_IN_AN_OVER).times.inject([over_commentary]) do |commentary, ball_number|
+      current_outcome = @outcomes[ball_number]
+      @target -= current_outcome.runs_scored
+      commentary = commentary << "#{@total_overs - @overs_left}.#{ball_number + 1} #{current_outcome.commentary}"
+      if @overs_left != 1 && (ball_number % 6) == 0
+        @overs_left -= 1
+      end
+      commentary
     end
+    @overs_left -= 1
+    commentary << over_commentary
+  end
+
+  private
+  def over_commentary
+    "#{@overs_left} overs left. #{@target} runs to win"
   end
 end
--- spec/score_card_spec.rb
+++ spec/score_card_spec.rb
@@ -4,13 +4,18 @@ require 'score_card'
 describe ScoreCard do
   describe '#generate_commentry' do
     it 'returns commentary of the outcomes' do
-      outcomes = [instance_double('Outcome', to_s: 'Kirat Boli scores 1 run'), instance_double('Outcome', to_s: 'NS Nodhi scores 4 runs'),
-                  instance_double('Outcome', to_s: 'NS Nodhi is out'), instance_double('Outcome', to_s: 'R Rumrah scores 1 run'),
-                  instance_double('Outcome', to_s: 'Kirat Boli scores 1 run'), instance_double('Outcome', to_s: 'R Rumrah scores 1 run')]
-      actual_commentary = ScoreCard.new(outcomes).commentary
+      outcomes = [instance_double('Outcome', runs_scored: 1, commentary: 'Kirat Boli scores 1 run'),
+                  instance_double('Outcome', runs_scored: 4, commentary: 'NS Nodhi scores 4 runs'),
+                  instance_double('Outcome', runs_scored: 0, commentary: 'NS Nodhi is out'),
+                  instance_double('Outcome', runs_scored: 1, commentary: 'R Rumrah scores 1 run'),
+                  instance_double('Outcome', runs_scored: 1, commentary: 'Kirat Boli scores 1 run'),
+                  instance_double('Outcome', runs_scored: 1, commentary: 'R Rumrah scores 1 run')]
+      number_of_overs = 1
+      target = 40
+      actual_commentary = ScoreCard.new(number_of_overs, target, outcomes).commentary
 
-      expected_commentary = ['0.1 Kirat Boli scores 1 run', '0.2 NS Nodhi scores 4 runs', '0.3 NS Nodhi is out',
-                             '0.4 R Rumrah scores 1 run', '0.5 Kirat Boli scores 1 run', '0.6 R Rumrah scores 1 run']
+      expected_commentary = ['1 overs left. 40 runs to win', '0.1 Kirat Boli scores 1 run', '0.2 NS Nodhi scores 4 runs', '0.3 NS Nodhi is out',
+                             '0.4 R Rumrah scores 1 run', '0.5 Kirat Boli scores 1 run', '0.6 R Rumrah scores 1 run', '0 overs left. 32 runs to win']
       expect(actual_commentary).to eq expected_commentary
     end
   end

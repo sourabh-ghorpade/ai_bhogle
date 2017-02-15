49c7b9025de4e455bfa6846c9f112e7b8da11df3
Sourabh Ghorpade <sourabh.ghorpade@runnr.in>
2017-02-15 23:48:22 +0530

    Now a score card can print a ball by ball summary.

--- lib/score_card.rb
+++ lib/score_card.rb
@@ -1,5 +1,11 @@
 class ScoreCard
   def initialize(outcomes)
+    @outcomes = outcomes
+  end
 
+  def commentary
+    @outcomes.each_with_index.map do |outcome, ball_number|
+      "0.#{ball_number + 1} #{outcome.to_s}"
+    end
   end
 end
--- spec/score_card_spec.rb
+++ spec/score_card_spec.rb
@@ -1,10 +1,17 @@
 require 'spec_helper'
 require 'score_card'
 
-
 describe ScoreCard do
-  describe '#add_outcome' do
-    it 'returns outcome of playing a ball' do
+  describe '#generate_commentry' do
+    it 'returns commentary of the outcomes' do
+      outcomes = [instance_double('Outcome', to_s: 'Kirat Boli scores 1 run'), instance_double('Outcome', to_s: 'NS Nodhi scores 4 runs'),
+                  instance_double('Outcome', to_s: 'NS Nodhi is out'), instance_double('Outcome', to_s: 'R Rumrah scores 1 run'),
+                  instance_double('Outcome', to_s: 'Kirat Boli scores 1 run'), instance_double('Outcome', to_s: 'R Rumrah scores 1 run')]
+      actual_commentary = ScoreCard.new(outcomes).commentary
+
+      expected_commentary = ['0.1 Kirat Boli scores 1 run', '0.2 NS Nodhi scores 4 runs', '0.3 NS Nodhi is out',
+                             '0.4 R Rumrah scores 1 run', '0.5 Kirat Boli scores 1 run', '0.6 R Rumrah scores 1 run']
+      expect(actual_commentary).to eq expected_commentary
     end
   end
 end

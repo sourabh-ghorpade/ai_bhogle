cc26317b0704c730d4180667485dc6639b867196
Sourabh Ghorpade <sourabh.ghorpade@runnr.in>
2017-02-19 10:42:56 +0530

    Now a team can return its scorecard.

--- lib/team.rb
+++ lib/team.rb
@@ -1,17 +1,17 @@
 class Team
   def initialize(all_batsmen, remaining_batsmen, striker, non_striker)
-    @batsmen = all_batsmen
+    @all_batsmen = all_batsmen
     @remaining_batsmen = remaining_batsmen
     @striker = striker
     @non_striker = non_striker
   end
 
   def score_card
-
+    ScoreCard.new(@all_batsmen)
   end
 
   def play(is_last_ball)
     outcome = @striker.play
-    return outcome, outcome.resultant_team(@batsmen, @remaining_batsmen, @striker, @non_striker, is_last_ball)
+    return outcome, outcome.resultant_team(@all_batsmen, @remaining_batsmen, @striker, @non_striker, is_last_ball)
   end
 end
--- spec/team_spec.rb
+++ spec/team_spec.rb
@@ -3,19 +3,21 @@ require 'team'
 require 'batsman'
 require 'outcomes/out_outcome'
 require 'outcomes/even_score_outcome'
+require 'score_card'
 
 describe Team do
+  let(:striker) { instance_double(Batsman) }
+  let(:non_striker) { instance_double(Batsman) }
+  let(:another_batsman) { instance_double(Batsman) }
+  let(:all_batsmen) { [striker, non_striker, another_batsman] }
+  let(:remaining_batsmen) { [another_batsman] }
+
   describe '#play' do
     it 'returns returns outcome of playing the ball and resultant team' do
-      striker = instance_double(Batsman)
-      non_striker = instance_double(Batsman)
-      another_batsman = instance_double(Batsman)
-      all_batsmen = [striker, non_striker, another_batsman]
       outcome = instance_double('Outcome')
       expect(striker).to receive(:play).and_return(outcome)
       expected_resultant_team = instance_double(Team)
       is_last_ball = true
-      remaining_batsmen = [another_batsman]
       expect(outcome).to receive(:resultant_team)
                              .with(all_batsmen, remaining_batsmen, striker, non_striker, is_last_ball)
                              .and_return(expected_resultant_team)
@@ -24,6 +26,17 @@ describe Team do
 
       expect(actual_outcome).to eq outcome
       expect(actual_team).to eq expected_resultant_team
+    end
+  end
+
+  describe '#score_card' do
+    it 'returns score card of current batsmen' do
+      expected_score_card = instance_double(ScoreCard)
+      expect(ScoreCard).to receive(:new).with(all_batsmen).and_return(expected_score_card)
+
+      actual_score_card = Team.new(all_batsmen, remaining_batsmen, striker, non_striker).score_card
+
+      expect(actual_score_card).to eq expected_score_card
     end
   end
 end
